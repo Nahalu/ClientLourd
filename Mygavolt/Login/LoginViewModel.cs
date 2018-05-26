22047e3b63d3595bfbf7084f358f9a4ec4b8bbe5
unknown <romain.mard@gmail.com>
2018-05-27 00:48:01 +0200

    AD

--- Mygavolt/Login/LoginViewModel.cs
+++ Mygavolt/Login/LoginViewModel.cs
@@ -1,54 +1,105 @@
-﻿using System.Windows;
-
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Mygavolt.Login
 {
     public class LoginViewModel : ObservableObject
     {
-
         public RelayCommand LoginCommandConnect { get; set; }
         public RelayCommand LoginCommandQuit { get; set; }
-
+        
         public LoginViewModel()
         {
-
             LoginCommandConnect = new RelayCommand(DoLogin);
-            LoginCommandQuit = new RelayCommand(QuitLogin);
+            LoginCommandQuit = new RelayCommand(o => ((Window)o).Close());
+
         }
 
-        private void DoLogin(object obj)
+        private string _Label = "";
+        public string Label
         {
- 
-            MainWindow app = new MainWindow();
-            MainWindowViewModel context = new MainWindowViewModel();
-            app.DataContext = context;
-            app.Show();
+            get
+            {
+                return _Label;
+            }
+            set
+            {
+                if (_Label != value)
+                {
+                    _Label = value;
+                }
+            }
         }
-
-        private void QuitLogin(object obj)
+        private string _Password = "";
+        public string Password
         {
-
-
+            get
+            {
+                return _Password;
+            }
+            set
+            {
+                if (_Password != value)
+                {
+                    _Password = value;
+                }
+            }
         }
 
-        /*
-        private void Connect(object sender, RoutedEventArgs e)
+        private void DoLogin(object a)
         {
+            DirectoryEntry Ldap = new DirectoryEntry();
+            Ldap.Path = "LDAP://anxium.fr/DC=mygavolt;DC=local";
+            Ldap.Username = "administrateur";
+            Ldap.Password = "apmq10*/59";
+            DirectorySearcher searcher = new DirectorySearcher(Ldap);
+            searcher.Filter = "(objectClass=user)";
 
+            foreach (SearchResult sr in searcher.FindAll())
+            {
+                YourType newRecord = ConvertToYourType(sr);
 
-            MainWindow app = new MainWindow();
-            MainWindowViewModel context = new MainWindowViewModel();
-            app.DataContext = context;
-            //app.ShowDialog();
-            app.Show();
+                if (Label == newRecord.givenName)
+                {
+                    MainWindow app = new MainWindow();
+                    MainWindowViewModel context = new MainWindowViewModel();
+                    app.DataContext = context;
+                    App.Current.MainWindow.Close();
+                    app.ShowDialog();
+                }
+                else
+                {
+
+                }
+            }
+
         }
-        private void Quit(object sender, RoutedEventArgs e)
+
+
+            public class YourType
         {
-            LoginView closeLogin = new LoginView();
-            closeLogin.Close();
+            public string givenName { get; set; }
 
         }
 
-        */
+        public YourType ConvertToYourType(SearchResult result)
+        {
+            YourType returnValue = new YourType();
+
+            if (result.Properties["givenName"] != null && result.Properties["givenName"].Count > 0)
+            {
+                returnValue.givenName = result.Properties["givenName"][0].ToString();
+            }
+
+            // ..... and so on for each of your values you need to extracxt
+
+            return returnValue;
+        }
+
+
     }
 }
+
